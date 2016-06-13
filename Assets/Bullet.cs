c2fe705bbfd5a5d473b62585d948d24661a6893b
caiwenchao <hustwenchao@gmail.com>
2016-06-13 17:25:42 +0800

    Add HealthBar and Combat

---
+++ Assets/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+    void OnCollisionEnter(Collision collision)
+    {
+        var hit = collision.gameObject;
+        var hitPlayer = hit.GetComponent<PlayerMove>();
+        if (hitPlayer != null)
+        {
+            var combat = hit.GetComponent<Combat>();
+            if(combat != null)
+            {
+                combat.TakeDamage(10);
+            }
+            Destroy(gameObject);
+        }
+    }
+}
